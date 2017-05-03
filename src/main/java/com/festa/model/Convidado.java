25ca39ab96619a2d3c727f266664103633dc8ffd
Marcelo Ponciano <ponciano@7rm.eb.mil.br>
2017-05-03 17:01:54 -0300

    first

---
+++ src/main/java/com/festa/model/Convidado.java
@@ -0,0 +1,48 @@
+package com.festa.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Convidado implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue
+	private Long Id;
+	
+	private String nome;
+	
+	private Integer quantidadeAcompanhantes;
+
+	public Long getId() {
+		return Id;
+	}
+
+	public void setId(Long id) {
+		Id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getQuantidadeAcompanhantes() {
+		return quantidadeAcompanhantes;
+	}
+
+	public void setQuantidadeAcompanhantes(Integer quantidadeAcompanhantes) {
+		this.quantidadeAcompanhantes = quantidadeAcompanhantes;
+	}
+	
+	
+
+}
